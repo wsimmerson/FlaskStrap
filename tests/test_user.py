0a7e12d8e3338db7bc57477fddcf99af70d178e5
Wayne Simmerson <wsimmerson@gmail.com>
2014-11-25 13:11:39 -0500

    user tests

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,3 +1,5 @@
+import unittest
+
 from test_base import BaseTestCase
 
 from flask import session
@@ -35,7 +37,7 @@ class UserTestCase(BaseTestCase):
             resp = self.login('joe@example.com', 'blow')
             self.assertIn(b'Username or Password is incorrect!', resp.data)
 
-    def test_404(self):
+    def test_404_not_found(self):
         with self.client:
             resp = self.client.get('some-page-that-doesnt-exist',
                                    follow_redirects=True)
@@ -61,6 +63,7 @@ class UserTestCase(BaseTestCase):
             self.assertIn(b'Login Required!', resp.data)
 
     def test_access_denied_for_basic_user(self):
+        with self.client:
             self.login('admin@email.com', 'admin')
             self.client.post('user/create',
                              data=dict(name='test user',
@@ -74,3 +77,58 @@ class UserTestCase(BaseTestCase):
             self.login('test@email.com', 'password')
             resp = self.client.get('user/create', follow_redirects=True)
             self.assertIn(b'ACCESS DENIED!', resp.data)
+
+    def test_user_list_page(self):
+        with self.client:
+            self.login('admin@email.com', 'admin')
+            resp = self.client.get('user/list', follow_redirects=True)
+            self.assertIn(b'admin@email.com', resp.data)
+
+    def test_view_other_user_profile(self):
+        with self.client:
+            self.login('admin@email.com', 'admin')
+            self.client.post('user/create',
+                             data=dict(name='test user',
+                                       email='test@email.com',
+                                       password='password',
+                                       password_confirm='password',
+                                       role='user'),
+                             follow_redirects=True,
+                             )
+            resp = self.client.get('user/profile/2')
+            self.assertIn(b'test user', resp.data)
+            self.assertNotIn(b'Old Password', resp.data)
+
+    def test_view_own_user_profile(self):
+        with self.client:
+            self.login('admin@email.com', 'admin')
+            resp = self.client.get('user/profile/1')
+            self.assertIn(b'Old Password', resp.data)
+
+    def test_delete_user(self):
+        with self.client:
+            self.login('admin@email.com', 'admin')
+            self.client.post('user/create',
+                             data=dict(name='test user',
+                                       email='test@email.com',
+                                       password='password',
+                                       password_confirm='password',
+                                       role='user'),
+                             follow_redirects=True,
+                             )
+            resp = self.client.get('user/delete/2', follow_redirects=True)
+            self.assertIn(b'test@email.com has been deleted', resp.data)
+
+    def test_edit_user(self):
+        with self.client:
+            self.login('admin@email.com', 'admin')
+            resp = self.client.post('user/edit/1',
+                                    data=dict(name='admin update',
+                                              email='admin@email.com',
+                                              role='admin'),
+                                    follow_redirects=True)
+            self.assertIn(b'User has been updated', resp.data)
+
+
+if __name__ == '__main__':
+    unittest.main()

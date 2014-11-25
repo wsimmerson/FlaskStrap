3dc55a2090eb61e70a9f9a3d5048d611e838dcd0
Wayne Simmerson <wsimmerson@gmail.com>
2014-11-24 19:08:16 -0500

    404/500 error handlers

--- project/__init__.py
+++ project/__init__.py
@@ -4,7 +4,7 @@
 
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 from flask.ext.sqlalchemy import SQLAlchemy
 
 #################
@@ -29,3 +29,14 @@ from project.home.controllers import home_bp
 
 app.register_blueprint(user_bp)
 app.register_blueprint(home_bp)
+
+# Basic HTTP error handling
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def indernal_error(error):
+    db.session.rollback()
+    flash("An unexpected error has occured.  The administrator has been notified", "error")
+    return render_template('404.html'), 500
